9dc1f95ee0796590a8afdaa8740331368830d363
SohelShahid117 <sohelshahid09@gmail.com>
2024-03-22 05:03:17 +0600

    complete module-37

--- notation.js
+++ notation.js
@@ -1,1 +1,25 @@
 //37-8 Dot Vs Bracket Notation
+const nayok = {
+    name :'sakib khan',
+    // naika : "mon mon"
+    age :35,
+    movies : ['dakar mastan',"golistaner gonda","balistaner tanda"],
+    profession : "ciNema",
+    25 :'summer',
+    'son-name' : "abul-khan"
+}
+
+const prof1 = nayok.profession;
+console.log(prof1);
+
+const prof2 = nayok['profession'];
+console.log(prof2)
+
+const boyos = 'age';
+console.log(nayok[boyos]);
+
+const seeson = '25';
+console.log(nayok[seeson]);
+
+const pola = 'son-name';
+console.log(nayok[pola]);
